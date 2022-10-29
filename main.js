717d5d251876fa3ff12ca9b9f5ea086fa27633cf
Anas Alnagar <35266228+oniseg@users.noreply.github.com>
2022-10-29 19:57:13 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+let title = document.querySelector('.title');
+let ul = document.querySelector('ul');
+let reload = document.querySelector('.reload');
+
+window.onload = function(){
+    if(window.navigator.onLine){
+        online()
+    }else{
+        offline()
+    }
+}
+
+window.addEventListener("online", function(){
+    online()
+});
+window.addEventListener("offline", function(){
+    offline()
+})
+reload.onclick = function(){
+    window.location.reload();
+}
+function online(){
+    title.innerHTML = "OnLine Now";
+    title.style.color = 'green';
+    ul.classList.add('hide');
+    reload.classList.add('hide');
+}
+function offline(){
+    title.innerHTML="offline Now";
+    title.style.color = '#666';
+    ul.classList.remove('hide');
+    reload.classList.remove('hide');
+}
+
